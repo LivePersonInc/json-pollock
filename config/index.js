11d39bd9e85d5d2feecdd5354c2766a887d6f68d
Meir Rotstein <meirr@liveperson.com>
2017-07-03 15:41:52 +0300

    fixing relative paths

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../editor/index.html'),
     assetsRoot: path.resolve(__dirname, '../editor'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/editor/',
+    assetsPublicPath: '/pages/WebAgent/json-pollock/editor/', //TODO make generic
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
