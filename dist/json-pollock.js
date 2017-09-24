25eda86f33d5d6ae0d912642d003807664586f83
Meir Rotstein <meirr@liveperson.com>
2017-09-24 14:48:05 +0300

    bump 1.0.20

--- dist/json-pollock.js
+++ dist/json-pollock.js
@@ -8171,7 +8171,7 @@ var instance = new _LPJsonPollock2.default();
 var init = instance.init.bind(instance);
 var render = instance.render.bind(instance);
 var registerAction = instance.registerAction.bind(instance);
-var version = '1.0.19';
+var version = '1.0.20';
 
 exports.init = init;
 exports.render = render;
