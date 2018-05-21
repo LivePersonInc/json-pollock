9c98de01b43af29a7730a151788f9a79fe8f198c
Meir Rotstein <meir@rotstein.co.il>
2018-05-21 21:23:23 +0300

    GitHubHeloper small refactor

--- src/GitHubHelper.js
+++ src/GitHubHelper.js
@@ -1,65 +1,71 @@
 import 'whatwg-fetch';
 import 'babel-polyfill';
+import { get } from 'lodash';
 
 const GITHUB_API = 'https://api.github.com';
 
 const STORAGE_KEYS = {
   GITHUB_TOKEN: 'jsonPollockPlaygroundGithubToken',
+  GITHUB_USER: 'GithubUser',
   GIST_PREFIX: 'jsonPollockPlayground',
 };
 
 class Gist {
-  constructor(name, content, url) {
+  constructor(name, content, url, ownerId) {
     this.name = name;
     this.content = content;
     this.url = url;
     this.isGist = true;
+    this.ownerId = ownerId;
   }
 }
 
-const getUserDetails = (token) => {
+const queryGitHubAPI = (token, path, storageKey) => {
+  const storageStr = localStorage.getItem(`${STORAGE_KEYS.GIST_PREFIX}_${storageKey}`);
+  let storageObj;
+  if (storageStr) {
+    storageObj = JSON.parse(storageStr);
+  }
   const options = { headers: { Authorization: `token ${token}` } };
-  return fetch(`${GITHUB_API}/user`, options).then((res) => {
-    if (res.status === 200) {
-      return res.json();
-    }
-    return '';
-  });
+  if (storageObj) {
+    options.headers['If-None-Match'] = storageObj.etag;
+  }
+
+  return fetch(`${GITHUB_API}/${path}`, options)
+    .then((res) => {
+      if (res.status === 304) { // not changed
+        return storageObj;
+      }
+
+      if (res.status === 200) {
+        const etag = res.headers.get('Etag');
+        return res.json().then((json) => {
+          if (storageKey) {
+            localStorage.setItem(`${STORAGE_KEYS.GIST_PREFIX}_${storageKey}`, JSON.stringify({ ...json, etag }));
+          }
+          return json;
+        });
+      }
+
+      return res.json().then(err => ({ ...err, error: true }));
+    });
 };
 
-const loadGist = (gistId, filename, token) => {
-  let storedGist;
-  const storedGistStr = localStorage.getItem(`${STORAGE_KEYS.GIST_PREFIX}_${gistId}`);
-  if (storedGistStr) {
-    storedGist = JSON.parse(storedGistStr);
-  }
-  const options = { headers: { Authorization: `token ${token}` } };
-  if (storedGist) {
-    options.headers['If-None-Match'] = storedGist.etag;
-  }
-  return fetch(`${GITHUB_API}/gists/${gistId}`, options)
-          .then((res) => {
-            if (res.status === 304) { // not changed
-              return { gist: storedGist };
-            }
-            const etag = res.headers.get('Etag');
-            return res.json().then((json => ({ gist: json, etag })));
-          })
-          .then((res) => {
-            const gist = res.gist;
-            if (res.etag) {
-              localStorage.setItem(`${STORAGE_KEYS.GIST_PREFIX}_${gistId}`, JSON.stringify({ ...gist, etag: res.etag }));
-            }
-            const files = gist.files && Object.keys(gist.files);
-            if (files && files.length) {
-              const file = filename && gist.files[filename] ?
-                gist.files[filename] :
-                gist.files[files[0]];
-              return new Gist(file.filename, file.content, gist.html_url);
-            }
-            return gist;
-          });
-};
+const getUserDetails = token => queryGitHubAPI(token, 'user', STORAGE_KEYS.GITHUB_USER).then(res => (res.error ? '' : res));
+
+const loadGist = (gistId, filename, token) =>
+  queryGitHubAPI(token, `gists/${gistId}`, gistId)
+    .then((gist) => {
+      const files = gist.files && Object.keys(gist.files);
+      if (files && files.length) {
+        const file = filename && gist.files[filename] ?
+          gist.files[filename] :
+          gist.files[files[0]];
+        return new Gist(file.filename, file.content, gist.html_url, get(gist, 'owner.id'));
+      }
+      return gist;
+    });
+
 
 const saveToken = (token) => {
   localStorage.setItem(STORAGE_KEYS.GITHUB_TOKEN, token);
