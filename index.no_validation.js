25769689b94c8a5d4c37558f577fbf961e8e0d8d
Meir Rotstein <meir@rotstein.co.il>
2018-07-05 12:32:55 +0300

    Introduce bundle which excludes validation

---
+++ index.no_validation.js
@@ -0,0 +1,24 @@
+// @flow
+// Stylesheets
+/*eslint-disable */
+import styles from './scss/style.scss';
+/*eslint-enable */
+import JsonPollock from './js/JsonPollock';
+
+const instance = new JsonPollock();
+
+const init = instance.init.bind(instance);
+const render = instance.render.bind(instance);
+const registerAction = instance.registerAction.bind(instance);
+const unregisterAction = instance.unregisterAction.bind(instance);
+const unregisterAllActions = instance.unregisterAllActions.bind(instance);
+const version = '@@VERSION';
+
+export {
+  init,
+  render,
+  registerAction,
+  unregisterAction,
+  unregisterAllActions,
+  version,
+};
---
+++ webpack.pack.no.umd.no.validation.conf.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const webpack = require('webpack');
+const FlowStatusWebpackPlugin = require('flow-status-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const pkgjson = require('./package.json');
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /index.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'string-replace-loader',
+          query: {
+            search: '@@VERSION',
+            replace: pkgjson.version,
+          },
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['flow', 'es2015', 'stage-0'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          use: [{
+            loader: 'css-loader',
+            options: { minimize: true },
+          }, {
+            loader: 'sass-loader',
+          }],
+          // use style-loader in development
+          fallback: 'style-loader',
+        }),
+      },
+    ],
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new FlowStatusWebpackPlugin({
+      failOnError: true,
+    }),
+    new ExtractTextPlugin('json-pollock.min.css'),
+    new webpack.optimize.UglifyJsPlugin({
+      include: /\.min\.js$/,
+      minimize: true,
+    }),
+  ],
+  entry: {    
+    'json-pollock.global.no_validation.min': './index.no_validation.js',
+  },
+  devtool: 'source-map',
+  output: {
+    filename: '[name].js',
+    library: 'JsonPollock',
+    libraryTarget: 'this',
+    publicPath: '/dist/',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
