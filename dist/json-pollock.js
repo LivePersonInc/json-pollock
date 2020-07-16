77c093fcfb8043a6efdf62f5cb669968c8730963
Meir Rotstein <meir@rotstein.co.il>
2020-07-16 12:55:00 +0300

    bump 1.4.4

--- dist/json-pollock.js
+++ dist/json-pollock.js
@@ -7928,7 +7928,7 @@ var registerAction = instance.registerAction.bind(instance);
 var unregisterAction = instance.unregisterAction.bind(instance);
 var unregisterAllActions = instance.unregisterAllActions.bind(instance);
 var validate = instance.validate.bind(instance);
-var version = '1.4.3';
+var version = '1.4.4';
 var TEMPLATE_TYPES = _JsonPollock2.default.TEMPLATE_TYPES;
 
 exports.init = init;
