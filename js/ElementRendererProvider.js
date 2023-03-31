d870132fc98faf371aae93d923049a7d50116781
Andrii Zhyrnov <zandruha29@gmail.com>
2023-03-31 16:29:08 +0300

    Revert "AB-418 removed redirect from map on mouse click"

--- js/ElementRendererProvider.js
+++ js/ElementRendererProvider.js
@@ -382,7 +382,6 @@ export default class ElementRendererProvider {
     });
 
     this.set(TYPES.MAP, (config): HTMLElement => {
-      const accessibilityWeb = config.accessibility && config.accessibility.web;
       const divEl = document.createElement('div');
       divEl.className = 'lp-json-pollock-element-map';
 
@@ -395,18 +394,9 @@ export default class ElementRendererProvider {
         divEl.style.cssText = Utils.styleToCss(config.style);
       }
 
-      if (accessibilityWeb) {
+      if (config.accessibility && config.accessibility.web) {
         Utils.appendAttributesFromObject(divEl, config.accessibility.web);
       }
-      if (!accessibilityWeb || !accessibilityWeb.tabindex) {
-        divEl.setAttribute('tabindex', '0');
-      }
-      divEl.onkeydown = (event) => {
-        event.preventDefault();
-        if (event.keyCode === 13 || event.keyCode === 32) {
-          window.open(`https://www.google.com/maps/search/?api=1&query=${config.la},${config.lo}`, '_blank');
-        }
-      };
 
       if (config.click && config.click.actions) {
         divEl.onclick = this.wrapAction(config.click);
